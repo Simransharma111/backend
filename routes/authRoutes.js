3f3322a6e3471d40e76500e630c507e2d7982ec1
Simran Sharma <ssa13386@gmail.com>
2024-09-23 22:10:04 +0530

    route changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser,logoutUser } = require('../controllers/authController');
 
-router.post('/register', registerUser);
+router.post('https://backend-gamma-nine-69.vercel.app/register', registerUser);
 router.post('/login', loginUser);
 router.delete('/logout', logoutUser);
 
